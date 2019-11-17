4032226f7a31aa1272c6532f0119bd79f63bc1cd
Jake Patterson <jake@patt.me>
2019-11-17 16:01:08 +0000

    Tests for shortest simplify-able line

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,7 +1,24 @@
-import { addOneToNumber } from '../src'
+import { Point, simplify } from '../src/index'
 
-describe('addOneToNumber', () => {
-  it('takes a number as an input and increments it by one', () => {
-    expect(addOneToNumber(1)).toEqual(2)
+describe('simplify', () => {
+  let points: Point[], simplified: Point[]
+
+  describe('shortest input (length=3)', () => {
+    beforeAll(() => {
+      points = [
+        { x: 0, y: 0 },
+        { x: 1, y: 1 },
+        { x: 2, y: 2 },
+      ]
+      simplified = simplify(points)
+    })
+
+    it('outputs a shorter array than the one inputted', () => {
+      expect(simplified.length).toBeLessThan(points.length)
+    })
+
+    it('removes the least important coordiate, accordint to the algorithm', () => {
+      expect(simplified.indexOf(points[1])).toEqual(-1)
+    })
   })
 })
